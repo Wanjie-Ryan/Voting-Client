8977f167174a0d2e9aa931ea6c70b34b03dbb615
Ryan <ryanwanjiedistortion@gmail.com>
2023-06-29 14:36:25 +0300

    voting

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,10 +1,59 @@
 import React from 'react'
 import './home.css'
-
+import image from '../assets/icon-votes.jpg'
 
 
 
 function Home() {
+
+    const aspirants = [
+
+        {
+            id:1,
+            image:{image},
+            name:'Ryan',
+            btn:'vote'
+        },
+
+        {
+            id:2,
+            image:'../assets/icon-votes.jpg',
+            name:'Rose',
+            btn:'vote'
+        },
+
+        {
+            id:3,
+            image:'../assets/icon-votes.jpg',
+            name:'Jimmy',
+            btn:'vote'
+        },
+
+        {
+            id:4,
+            image:'../assets/icon-votes.jpg',
+            name:'Kale',
+            btn:'vote'
+        },
+
+        {
+            id:5,
+            image:'../assets/icon-votes.jpg',
+            name:'Juliet',
+            btn:'vote'
+        },
+
+        {
+            id:6,
+            image:'../assets/icon-votes.jpg',
+            name:'Kim',
+            btn:'vote'
+        },
+
+
+
+
+    ]
 
 
   return (
@@ -15,7 +64,26 @@ function Home() {
 
         <section className="voters">
 
-            <h1 className="aspirants">And Our Aspirants are</h1>
+            <h1 className="aspirants-title">And Our Aspirants are</h1>
+
+            {aspirants.map((person)=>(
+
+
+                
+                <div className ='aspirant-containers' key ={person.id}>
+
+                    <div className="aspirant-cont">
+
+                        <img className ='asp-img' src ={person.image} alt ={person.name}/>
+                        <h3 className ='asp-name'>{person.name}</h3>
+                        <button className="btn-cont">{person.btn}</button>
+
+
+                    </div>
+
+            </div>
+
+            ))}
 
 
 
