89c49912f29629faf3bfaae943e240290160618d
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-10 13:34:33 +0300

    voting-integration

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -11,66 +11,110 @@ import {TbFidgetSpinner} from 'react-icons/tb'
 
 function Home() {
 
-    const aspirants = [
+    // const aspirants = [
 
-        {
-            id:1,
-            image:image,
-            name:'Ryan',
-            position:'Delegate',
-            school:'Engineering and Technology',
-            btn:'vote'
-        },
+    //     {
+    //         id:1,
+    //         image:image,
+    //         name:'Ryan',
+    //         position:'Delegate',
+    //         school:'Engineering and Technology',
+    //         btn:'vote'
+    //     },
 
-        {
-            id:2,
-            image:image,
-            name:'Rose',
-            position:'Delegate',
-            school:'Engineering and Technology',
-            btn:'vote'
-        },
+    //     {
+    //         id:2,
+    //         image:image,
+    //         name:'Rose',
+    //         position:'Delegate',
+    //         school:'Engineering and Technology',
+    //         btn:'vote'
+    //     },
 
-        {
-            id:3,
-            image:image,
-            name:'Jimmy',
-            position:'Delegate',
-            school:'Engineering and Technology',
-            btn:'vote'
-        },
+    //     {
+    //         id:3,
+    //         image:image,
+    //         name:'Jimmy',
+    //         position:'Delegate',
+    //         school:'Engineering and Technology',
+    //         btn:'vote'
+    //     },
 
-        {
-            id:4,
-            image:image,
-            name:'Kale',
-            position:'Delegate',
-            school:'Engineering and Technology',
-            btn:'vote'
-        },
+    //     {
+    //         id:4,
+    //         image:image,
+    //         name:'Kale',
+    //         position:'Delegate',
+    //         school:'Engineering and Technology',
+    //         btn:'vote'
+    //     },
 
-        {
-            id:5,
-            image:image,
-            name:'Juliet',
-            position:'Delegate',
-            school:'Engineering and Technology',
-            btn:'vote'
-        },
+    //     {
+    //         id:5,
+    //         image:image,
+    //         name:'Juliet',
+    //         position:'Delegate',
+    //         school:'Engineering and Technology',
+    //         btn:'vote'
+    //     },
 
-        {
-            id:6,
-            image:image,
-            name:'Kim',
-            position:'Delegate',
-            school:'Engineering and Technology',
-            btn:'vote'
-        },
-
-
+    //     {
+    //         id:6,
+    //         image:image,
+    //         name:'Kim',
+    //         position:'Delegate',
+    //         school:'Engineering and Technology',
+    //         btn:'vote'
+    //     },
 
 
-    ]
+
+
+    // ]
+
+    const [loading, setloading] = useState(false)
+    const [errmsg, seterrmsg] = useState('')
+    const [aspirants, setaspirants] = useState([])
+
+    useEffect(()=>{
+
+        const fetchAspirants = async()=>{
+
+            try{
+
+                setloading(true)
+
+                const fetchedAsps = await axios.get('http://localhost:3007/api/admin/allaspirants')
+
+                console.log(fetchedAsps)
+
+                const {allaspirants} = fetchedAsps.data
+
+                console.log(allaspirants)
+
+                setaspirants(allaspirants)
+
+                setloading(false)
+
+            }
+
+            catch(err){
+
+                console.log(err)
+                seterrmsg('There seems to be an error please refresh the page and try again')
+                setloading(false)
+
+
+            }
+
+
+        }
+
+        fetchAspirants()
+
+
+
+    },[])
 
     // const navigate = useNavigate()
 
@@ -96,7 +140,7 @@ function Home() {
 
                     {aspirants.map((person)=>(
                         
-                        <div className ='aspirant-containers' key ={person.id}>
+                        <div className ='aspirant-containers' key ={person._id}>
 
                             <div className="aspirant-cont">
 
@@ -105,12 +149,12 @@ function Home() {
                                 <div className ='aspirant-details'>
 
                                     <h3 className ='asp-name'> Aspirant Name:{person.name}</h3>
-                                    <h3 className ='asp-name'> Aspirant Position:{person.position}</h3>
-                                    <h3 className ='asp-name'> Aspirant School:{person.school}</h3>
+                                    <h3 className ='asp-name'> Aspirant Position:{person.Position}</h3>
+                                    <h3 className ='asp-name'> Aspirant School:{person.Represent}</h3>
 
-                                    <Link to= {{ pathname: `/voter/${person.id}`  }} >
+                                    <Link to= {{ pathname: `/voter/${person._id}`  }} >
 
-                                     <button className="btn-cont" >{person.btn}</button>
+                                     <button className="btn-cont" >Vote</button>
                                     
                                     </Link>
 
