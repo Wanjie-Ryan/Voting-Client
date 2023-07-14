333786f3b03ba79c4191be284478b9ec00748a66
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-14 09:29:33 +0300

    aspirant-integration

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/navbar/navbar'
 import Homepages from '../../components/home/home'
-import Footer from '../../components/footer/Footer'
+import Footer from '../../components/footer/footer'
 
 
 
--- src/pages/voter/voter.jsx
+++ src/pages/voter/voter.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Navbar from '../../components/navbar/navbar'
 import VoterSpecific from '../../components/voter-specific/voter'
-import Footer from '../../components/footer/Footer'
+// import Footer from '../../components/footer/Footer'
+import Footer from '../../components/footer/footer'
 
 
 
